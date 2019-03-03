b4cf7ce4f41f1a810cc24b72c6f474b1e23ed676
tulasi <tulasik788@gmail.com>
2019-03-03 21:28:49 +0530

    my gitgit project added to remote

---
+++ gitgit/src/main/java/com/slokam/gitgit/GitgitApplication.java
@@ -0,0 +1,13 @@
+package com.slokam.gitgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgitApplication.class, args);
+	}
+
+}
